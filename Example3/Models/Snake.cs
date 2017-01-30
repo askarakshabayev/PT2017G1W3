54e7821da3b27adfc9d2568ccac0219732ed622d
unknown <a.akshabaev@c441a-10.kbtu.kz>
2017-01-30 11:58:59 +0600

    snake example

---
+++ Example3/Models/Snake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3.Models
+{
+    class Snake
+    {
+        public List<Point> body;
+        public char sign;
+        public ConsoleColor color;
+
+        public Snake()
+        {
+            body = new List<Point>();
+            body.Add(new Point(10, 10));
+            sign = '*';
+            color = ConsoleColor.Yellow;
+        }
+
+        public void move(int dx, int dy)
+        {
+            for (int i = 1; i < body.Count; i++)
+            {
+                body[i].x = body[i - 1].x;
+                body[i].y = body[i - 1].y;
+            }
+            body[0].x += dx;
+            body[0].y += dy;
+        }
+
+        public void addItem() {
+            Point p = new Point(0, 0);
+            body.Add(p);
+        }
+    }
+}
---
+++ Example3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Models.Snake snake = new Models.Snake();
+            int cnt = 0;
+            while (true)
+            {
+                cnt++;
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.UpArrow)
+                    snake.move(0, -1);
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                    snake.move(0, 1);
+                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                    snake.move(-1, 0);
+                if (pressedKey.Key == ConsoleKey.RightArrow)
+                    snake.move(1, 0);
+                //Console.Clear();
+                for (int i = 0; i < snake.body.Count; i++)
+                {
+                    Console.SetCursorPosition(snake.body[i].x, snake.body[i].y);
+                    Console.Write(snake.sign);
+                    Console.Write(snake.body.Count);
+                }
+                if (cnt % 10 == 0)
+                    snake.addItem();
+            }
+        }
+    }
+}
