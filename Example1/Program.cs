78d3534089b02cb8fe4baf31bcc37d76037d8116
unknown <a.akshabaev@c441a-10.kbtu.kz>
2017-01-30 10:55:37 +0600

    examples of week3 added

---
+++ Example1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            while (true)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.WriteLine("Left");
+                }
+                if (pressedKey.Key == ConsoleKey.RightArrow)
+                {
+                    Console.WriteLine("Right");
+                }
+                if (pressedKey.Key == ConsoleKey.UpArrow)
+                {
+                    Console.WriteLine("Up");
+                }
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                {
+                    Console.WriteLine("Down");
+                }
+                if (pressedKey.Key == ConsoleKey.Escape)
+                    break;
+                if (pressedKey.Key == ConsoleKey.Enter)
+                    Console.Clear();
+            }
+
+        }
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Example2
+{
+    class Program
+    {
+        static void ShowInfo(DirectoryInfo directory, int cursor)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            int index = 0;
+            foreach (FileSystemInfo fInfo in directory.GetFileSystemInfos())
+            {
+                if (index == cursor)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else
+                    Console.ForegroundColor = ConsoleColor.Red;
+                index++;
+                if (fInfo.GetType() == typeof(FileInfo))
+                    Console.Write("File: ");
+                else
+                    Console.Write("Directory: ");
+                Console.WriteLine(fInfo.Name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int cursor = 0;
+            DirectoryInfo directory = new DirectoryInfo(@"C:\TEMP\BCB6");
+            while (true)
+            {
+                Console.Clear();
+                ShowInfo(directory, cursor);
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.UpArrow)
+                    if (cursor > 0)
+                        cursor--;
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                    if (cursor < directory.GetFileSystemInfos().Length - 1)
+                        cursor++;
+                if (pressedKey.Key == ConsoleKey.Enter)
+                {
+                    FileSystemInfo fi = directory.GetFileSystemInfos()[cursor];
+                    directory = new DirectoryInfo(fi.FullName);
+                }
+                if (pressedKey.Key == ConsoleKey.Backspace)
+                {
+                    try
+                    {
+                        directory = Directory.GetParent(directory.FullName);
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+                if (pressedKey.Key == ConsoleKey.Escape)
+                    break;
+            }
+        }
+    }
+}
